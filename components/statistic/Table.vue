c03369bad542b8b1b2b86d246a5e0edfab739fdb
WarGhost132 <wawghostick@gmail.com>
2025-02-21 12:36:03 +0300

    fix scroll to statistic page

--- components/statistic/Table.vue
+++ components/statistic/Table.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="overflow-auto relative">
+  <div class="relative">
     <SkeletonLoader 
       v-if="loading" 
       type="table" 
@@ -7,31 +7,33 @@
       containerClass="w-full space-y-2"
     />
 
-    <table v-else-if="statistics.length > 0" class="table-fixed w-full text-xl border-separate border-spacing-y-2.5">
-      <thead>
-        <tr class="shadow-sm rounded-lg">
-          <th class="table-header rounded-s-lg">Порядковый номер</th>
-          <th class="table-header">Дата последнего сообщения</th>
-          <th class="table-header">Тип чата</th>
-          <th class="table-header rounded-e-lg">Избранное</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr 
-          v-for="item in statistics" 
-          :key="item.id"
-          class="bg-slate-100 hover:!bg-slate-200 cursor-pointer"
-          @click="onClickDialog(String(item.id))"
-        >
-          <td class="table-body rounded-s-lg">{{ item.id }}</td>
-          <td class="table-body">
-            <time :datetime="$dayjs(item.date_last_message).format('DD.MM.YYYY')">{{ $dayjs(item.date_last_message).format('DD.MM.YYYY') }}</time>
-          </td>
-          <td class="table-body">{{ item.chat_type }}</td>
-          <td class="table-body rounded-e-lg">{{ item.note }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div v-else-if="statistics.length > 0" class="h-[600px] overflow-y-auto table-container pr-2">
+      <table  class="table-fixed w-full text-xl border-separate border-spacing-y-2.5">
+        <thead class="sticky top-0 bg-slate-300">
+          <tr class="shadow-sm rounded-lg">
+            <th class="table-header rounded-s-lg">Порядковый номер</th>
+            <th class="table-header">Дата последнего сообщения</th>
+            <th class="table-header">Тип чата</th>
+            <th class="table-header rounded-e-lg">Избранное</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr 
+            v-for="item in statistics" 
+            :key="item.id"
+            class="bg-slate-100 hover:!bg-slate-200 cursor-pointer"
+            @click="onClickDialog(String(item.id))"
+          >
+            <td class="table-body rounded-s-lg">{{ item.id }}</td>
+            <td class="table-body">
+              <time :datetime="$dayjs(item.date_last_message).format('DD.MM.YYYY')">{{ $dayjs(item.date_last_message).format('DD.MM.YYYY') }}</time>
+            </td>
+            <td class="table-body">{{ item.chat_type }}</td>
+            <td class="table-body rounded-e-lg">{{ item.note }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 
     <div v-else class="flex justify-center">
       <h3 class="text-xl">Список диалогов пуст</h3>
@@ -64,5 +66,20 @@ const onClickDialog = (id: string) => {
 }
 .table-body {
   @apply p-6 border-b text-left
+}
+.table-container::-webkit-scrollbar {
+  @apply w-2
+}
+
+.table-container::-webkit-scrollbar-track {
+  @apply bg-gray-50 rounded-lg
+}
+
+.table-container::-webkit-scrollbar-thumb {
+  @apply rounded-lg bg-slate-700
+}
+
+.table-container::-webkit-scrollbar-thumb:hover {
+  @apply bg-slate-600
 }
 </style>
